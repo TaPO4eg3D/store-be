cb5165069b02ee79704c220bbcedc34d7af8c9a6
Denis Fomin <dfomin@retechlabs.com>
2021-10-28 22:55:50 +0300

    (feat) Add docker for local development

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,8 +92,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'postgres',
+        'PORT': '5432',
+
     }
 }
 
