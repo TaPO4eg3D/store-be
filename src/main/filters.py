d9a4629db676329b063f680fb9d0e13dd7bcee5b
Denis Fomin <dfomin@retechlabs.com>
2021-10-28 21:57:25 +0300

    (feat) Add filter on IDs for Product view

--- src/main/filters.py
+++ src/main/filters.py
@@ -1,9 +1,16 @@
 from django_filters import rest_framework as filters
+from django_filters.filters import BaseInFilter, NumberFilter, BaseInFilter
 
 from . import models
 
 
+class NumberInFilter(BaseInFilter, NumberFilter):
+    pass
+
+
 class ProductFilter(filters.FilterSet):
+    id = NumberInFilter(field_name='id', lookup_expr='in')
+
     class Meta:
         model = models.Product
         fields = (
